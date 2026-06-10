f6892189ded72e118a0b647f2546c446bf0c7992
gultomamsal <gultomamsal@gmail.com>
2026-06-10 11:03:01 +0700

    up

--- clickhouse/06_preagg.sql
+++ clickhouse/06_preagg.sql
@@ -158,7 +158,6 @@ SELECT
     count_date,
     disposition,
     countMerge(count)      AS total,
-    round(sum(avg_talk_sec) / nullIf(countMerge(count), 0), 2) AS avg_talk_seconds,
     branch_code
 FROM agg_disposition_summary
 GROUP BY count_date, disposition, branch_code
